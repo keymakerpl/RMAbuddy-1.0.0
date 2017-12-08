a1c4bac90c585d520761e3825fe2173973608474
Radek <serwis@erkomserwis.pl>
2017-12-08 14:41:38 +0100

    NewFeature branch

--- src/controller/SettingsViewController.java
+++ src/controller/SettingsViewController.java
@@ -22,6 +22,8 @@ public class SettingsViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+		
+		//NewFeature branch
     }    
     
 }
